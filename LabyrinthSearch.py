c2806aa823362a5202ac2a0f6c58168860e48e68
yannic-b <5boysen@informatik.uni-hamburg.de>
2017-11-02 13:54:37 +0100

    Erste Lösungsversion

--- LabyrinthSearch.py
+++ LabyrinthSearch.py
@@ -4,5 +4,35 @@ LabyrinthSearch
 by Thomas Hofmann and Yannic Boysen
 """
 
-###Blatt 3:
+from __future__ import print_function
 
+###Blatt 3:
+def importLabyrinth(file):
+    environment = open(file, "r")
+    labyrinth = []
+    for y, line in enumerate(environment):
+        l = []
+        for x, c in enumerate(line):
+            if c == 'x':
+                l.append(0)
+            else:
+                l.append(1)
+                if c == 's':
+                    startCoords = (x+1,y+1)
+                else:
+                    goalCoords = (x + 1, y + 1)
+        labyrinth.append(l)
+    environment.close()
+    return (labyrinth, startCoords, goalCoords)
+
+def printLabyrinth(labyrinth):
+    for row in labyrinth:
+        for c in row:
+            if c:
+                print(" ", end=" ")
+            else:
+                print("x", end=" ")
+        print("\n")
+
+lab1 = importLabyrinth("ev1.txt")
+printLabyrinth(lab1[0])
