624b5c389e7df4df379be832bc3192646701d61c
yannic-b <5boysen@informatik.uni-hamburg.de>
2017-11-06 01:42:07 +0100

    Blatt 3 Update und Blatt 4 WIP

--- LabyrinthSearch.py
+++ LabyrinthSearch.py
@@ -1,38 +1,76 @@
 """
 GWV-WS1718:
-LabyrinthSearch
+LabyrinthSearch (Python 2.7)
 by Thomas Hofmann and Yannic Boysen
 """
 
+# to use end functionallity
 from __future__ import print_function
+# for future exercises
+import Queue
+import numpy as np
 
-###Blatt 3:
+# Blatt 3:
 def importLabyrinth(file):
+    # load file with labyrinth information
     environment = open(file, "r")
     labyrinth = []
+    # iterate through the lab and save 0 for every x and 1 for every free spot / start / end.
     for y, line in enumerate(environment):
-        l = []
-        for x, c in enumerate(line):
-            if c == 'x':
-                l.append(0)
+        row = []
+        for x, character in enumerate(line):
+            if character == 'x':
+                row.append(0)
             else:
-                l.append(1)
-                if c == 's':
-                    startCoords = (x+1,y+1)
-                else:
-                    goalCoords = (x + 1, y + 1)
-        labyrinth.append(l)
+                # save start and goal
+                if character == 's':
+                    startCoords = [x + 1, y + 1]
+                elif character == 'g':
+                    goalCoords = [x + 1, y + 1]
+                row.append(1)
+        # append whole row to array
+        labyrinth.append(row)
     environment.close()
-    return (labyrinth, startCoords, goalCoords)
+    return labyrinth, startCoords, goalCoords
 
-def printLabyrinth(labyrinth):
-    for row in labyrinth:
-        for c in row:
-            if c:
+def printLabyrinth(lab, robot = (0, [0, 0])):
+    labyrinth, start, goal = lab
+    # iterate through the internal structure (2d array)
+    for y, row in enumerate(labyrinth):
+        for x, c in enumerate(row):
+            #print different symbols for ASCII representation
+            if robot[0] & (robot[1] == [x+1, y+1]):   # optional robot
+                print("r", end=" ")
+            elif (start == [x+1, y+1]):
+                print("s", end=" ")
+            elif (goal == [x+1, y+1]):
+                print("g", end=" ")
+            elif c:
                 print(" ", end=" ")
             else:
                 print("x", end=" ")
         print("\n")
 
+# testing functionality
 lab1 = importLabyrinth("ev1.txt")
-printLabyrinth(lab1[0])
+printLabyrinth(lab1)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Blatt 4: WIP
+def bfs(lab):
+    labyrinth, start, goal = lab
+    q = Queue()
+    q.enqueue(start)
+    while q:
+        front = q.dequeue()
+        if front == goal:
+            return q
+        for node in getNext(front):
+            print()
+
+def getNext(front):
+    nextNodes = []
+    additionTuples = np.array([[0,-1], [-1,0], [0,1], [1,0]])
+    for t in additionTuples:
+        print()
