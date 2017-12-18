8ecfcde12526f33d812284078cab717c08f6e33a
yannic-b <5boysen@informatik.uni-hamburg.de>
2017-12-18 03:08:59 +0100

    Commit

--- Markov/MarkovChain_T.py
+++ Markov/MarkovChain_T.py
@@ -2,22 +2,23 @@ import random
 
 # import data
 text_file = 'ggcc-one-word-per-line.txt'
-f = open (text_file, 'r')
+f = open(text_file, 'r')
 wordset = f.readlines()
 f.close()
 wordset = [word.strip() for word in wordset]
 
+
 # operators is number of words observed
 # operator = 1 should be a bigram
 def createProbabilityDict(words, operator):
-    next_occurence_dict = dict()
-    for i in range (0, len(words) - operator):
-        if (words[i] not in next_occurence_dict):
-            next_occurence_dict[words[i]] = []
+    next_occurrence_dict = dict()
+    for i in range(0, len(words) - operator):
+        if words[i] not in next_occurrence_dict:
+            next_occurrence_dict[words[i]] = []
 
-        for o in range (1, operator):
-            next_occurence_dict[words[i]].append(words[i + o])
-    return next_occurence_dict
+        for o in range(1, operator):
+            next_occurrence_dict[words[i]].append(words[i + o])
+    return next_occurrence_dict
 
 
 # generates random sequence with given length and start position
@@ -25,14 +26,14 @@ def generateRandomSequence(length, startPosition, wordset):
     startWord = wordset[startPosition]
     sequence = startWord + ' '
 
-    for i in range (0, length):
+    for i in range(0, length):
         nextWord = random.choice(dictionary[startWord]) + ' '
         sequence += nextWord
 
     return sequence
 
 
-#for testing purposes
+# for testing purposes
 dictionary = createProbabilityDict(wordset, 6)
-print(generateRandomSequence(25, 500, wordset))
-
+for i in range(20):
+    print(generateRandomSequence(25, 500, wordset))
