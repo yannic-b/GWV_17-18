b801387825287aa4227b025a275cfa0cce0f795c
yannic-b <5boysen@informatik.uni-hamburg.de>
2018-01-24 22:51:11 +0100

    angefangen ein volles Spiel zu simulieren

--- Projekt/yahtzee.py
+++ Projekt/yahtzee.py
@@ -8,7 +8,7 @@ Thomas Hofmann, Yannic Boysen
 """
 
 import random
-from collections import Counter
+# from collections import Counter
 
 
 class Yahtzee:
@@ -17,6 +17,27 @@ class Yahtzee:
         self.game = []
         self.dices = Dices()
         self.score_sheet = ScoreSheet()
+        self.dictOfOp = {}
+
+    def simulateGame(self, verbosity=0):
+        x = 0
+        while x < 5:  # self.score_sheet.isFilled():
+            self.dices = Dices()
+            self.dices.diceToString()
+            keep = random.sample(self.dices.dices, random.randint(1, 5))
+            print keep
+            self.dices.rollAgain(keep)
+            self.dices.diceToString()
+            keep = random.sample(self.dices.dices, random.randint(1, 5))
+            print keep
+            self.dices.rollAgain(keep)
+            self.dices.diceToString()
+            self.checkAllOpt()
+            maxOpt = max(self.dictOfOp, key=self.dictOfOp.get)
+            if maxOpt in self.score_sheet.upperScores.keys():
+                self.score_sheet.addThrow(self.dices, "upper", maxOpt)
+                
+            x += 1
 
 
     def digitScore(self, digit):
@@ -28,29 +49,25 @@ class Yahtzee:
 
     def checkAllOpt(self):
         # dictOfOpt holds all possible Options and their utility (points)
-        dictOfOpt = {}
-        dictOfOpt.update((self.checkDups()))
-        dictOfOpt.update(self.checkFullHouse())
-        dictOfOpt.update(self.checkYahtzee())
-        dictOfOpt.update(self.checkThreeOfAKind())
-        dictOfOpt.update(self.checkFourOfAKind())
-        dictOfOpt.update(self.checkSmallStreet())
-        dictOfOpt.update(self.checkLargeStreet())
-        print dictOfOpt
-
-
+        self.dictOfOpt.update((self.checkDups()))
+        self.dictOfOpt.update(self.checkFullHouse())
+        self.dictOfOpt.update(self.checkYahtzee())
+        self.dictOfOpt.update(self.checkThreeOfAKind())
+        self.dictOfOpt.update(self.checkFourOfAKind())
+        self.dictOfOpt.update(self.checkSmallStreet())
+        self.dictOfOpt.update(self.checkLargeStreet())
 
     # Checks for Dups and adds them with their Utility into a dict allDups.
     def checkDups(self):
         allDups = {}
         for key, value in self.dices.counts.iteritems():
-            if value > 1 and self.score_sheet.upperScores.values()[key - 1] == None:
+            if value > 1 and self.score_sheet.upperScores.values()[key - 1] is None:
                 valUtilitiy = key * value
                 allDups[key] = valUtilitiy
         return allDups
 
     def checkFullHouse(self):
-        if self.score_sheet.lowerScores['fh'] == None:
+        if self.score_sheet.lowerScores['fh'] is None:
             triple = False
             double = False
             for value in self.dices.counts.itervalues():
@@ -58,29 +75,29 @@ class Yahtzee:
                     double = True
                 elif value == 3:
                     triple = True
-            if triple == True and double == True:
-                return {'fh' : 25}
+            if triple and double:
+                return {'fh': 25}
             else:
-                return {'fh' : 0}
+                return {'fh': 0}
         else:
             return {'fh': 0}
 
     def checkYahtzee(self):
-        if self.score_sheet.lowerScores['y'] == None:
+        if self.score_sheet.lowerScores['y'] is None:
             firstNumber = self.dices.dices[0]
             counter = 0
             for number in self.dices.dices:
                 if number == firstNumber:
                     counter += 1
             if counter == 5:
-                return {'y' : 50}
+                return {'y': 50}
             else:
-                return {'y' : 0}
+                return {'y': 0}
         else:
             return {'y': 0}
 
     def checkThreeOfAKind(self):
-        if self.score_sheet.lowerScores['3oak'] == None:
+        if self.score_sheet.lowerScores['3oak'] is None:
             for value in self.dices.counts.itervalues():
                 if value > 2:
                     return {"3oak" : sum(self.dices.dices)}
@@ -89,7 +106,7 @@ class Yahtzee:
             return {"3oak": 0}
 
     def checkFourOfAKind(self):
-        if self.score_sheet.lowerScores['4oak'] == None:
+        if self.score_sheet.lowerScores['4oak'] is None:
             for value in self.dices.counts.itervalues():
                 if value > 3:
                     return {"4oak" : sum(self.dices.dices)}
@@ -98,7 +115,7 @@ class Yahtzee:
             return {"4oak": 0}
 
     def checkSmallStreet(self):
-        if self.score_sheet.lowerScores['sst'] == None:
+        if self.score_sheet.lowerScores['sst'] is None:
             counters = []
             counter = 0
             for x in range(1,5):
@@ -122,10 +139,8 @@ class Yahtzee:
         else:
             return {"sst": 0}
 
-
-
     def checkLargeStreet(self):
-        if self.score_sheet.lowerScores['lst'] == None:
+        if self.score_sheet.lowerScores['lst'] is None:
             counters = []
             counter = 0
             for x in range(1,6):
@@ -145,15 +160,10 @@ class Yahtzee:
             return {"lst": 0}
 
 
-
-
-
-
-
 class Dices:
 
     def __init__(self):
-        self.dices = [random.randint(1, 6) for i in range(5)]
+        self.dices = sorted([random.randint(1, 6) for i in range(5)])
         self.update()
 
     def update(self):
@@ -164,14 +174,18 @@ class Dices:
         self.fives = self.dices.count(5)
         self.sixes = self.dices.count(6)
         self.counts = {}  # dict(Counter(self.dices))
-        for x in range(1,7):
+        for x in range(1, 7):
             self.counts[x] = self.dices.count(x)
 
     def diceToString(self):
         print self.dices
 
     def rollAgain(self, keep):
-        self.dices = keep + [random.randint(1, 6) for i in range(5 - len(keep))]
+        # remove = [d for d in self.dices if d not in keep]
+        for d in keep:
+            if d not in self.dices:
+                keep.remove(d)
+        self.dices = sorted(keep + [random.randint(1, 6) for i in range(5 - len(keep))])
         self.update()
 
     def countOf(self, digit):
@@ -201,10 +215,15 @@ class ScoreSheet:
             'y': None,
             'c': None
         }
+
         self.total_all = 0
         self.total_upper = 0
         self.total_lower = 0
         self.bonus = False
+
+    def isFilled(self):
+        board = [self.upperScores.values() + self.lowerScores.values()]
+        return all(v is not None for v in board)
 
     def calcTotal(self):
         for key, value in self.upperScores.iteritems():
@@ -225,18 +244,13 @@ class ScoreSheet:
             return True
 
 
-
-
-
-
-
-
-#d = Dices()
 y = Yahtzee()
-print y.dices.counts
-y.checkAllOpt()
+# print y.dices.counts
+# y.checkAllOpt()
 
-#s = ScoreSheet()
-#s.addThrow(d, 'upper', '5')
-#s.calcTotal()
-#print s.total
+# s = ScoreSheet()
+# s.addThrow(d, 'upper', '5')
+# s.calcTotal()
+# print s.total
+
+y.simulateGame()
